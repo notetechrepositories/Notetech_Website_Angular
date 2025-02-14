b09ab38958e5d052ac039cd6cdef5ac98bd96a1a
prathaphari@notetech.com <prathaphari@notetech.com>
2025-02-14 21:11:50 +0530

    update

--- src/app/View/Layout/full/full.component.ts
+++ src/app/View/Layout/full/full.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener,Renderer2  } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-full',
@@ -8,23 +8,38 @@ import { Component, HostListener,Renderer2  } from '@angular/core';
 export class FullComponent {
   isMenuOpen = false;
   isScrolled = false;
+  footerData = {
+    whatsapp: "https://wa.me/919895046902",
+    facebook: "https://www.facebook.com",
+    twitter: "https://twitter.com/Notetech_IT",
+    github: "",
+    linkedin: "https://www.linkedin.com/company/notetech-software/",
+    instagram: "https://www.instagram.com/notetechsoftware/",
+    skype: "https://join.skype.com/invite/xXRXvR3Qpoyo",
+    copyRights: "2025 Notetech Software.All rights reserved.",
+    companyName: "Notetech Software",
+    footerDescription: "Creating beautiful digital experiences with modern design solutions for forward - thinking companies."
+  }
 
-  constructor(private renderer: Renderer2){}
+  headerData = {
+    headerlogo: "assets/images/notetech-logo-light.png"
+  }
+  constructor(private renderer: Renderer2) { }
 
-    @HostListener('window:scroll', [])
-    onWindowScroll() {
-      this.isScrolled = window.scrollY > 100; // Change threshold as needed
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 100; // Change threshold as needed
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    if (this.isMenuOpen) {
+      // Disable scrolling
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      // Enable scrolling
+      this.renderer.removeStyle(document.body, 'overflow');
     }
-  
-    toggleMenu() {
-      this.isMenuOpen = !this.isMenuOpen;
-  
-      if (this.isMenuOpen) {
-        // Disable scrolling
-        this.renderer.setStyle(document.body, 'overflow', 'hidden');
-      } else {
-        // Enable scrolling
-        this.renderer.removeStyle(document.body, 'overflow');
-      }
-    }
+  }
 }
--- src/app/View/Pages/home/home.component.ts
+++ src/app/View/Pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +9,54 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 export class HomeComponent {
   glowIntensity: number = 1;
   startCounter = false; // Flag to activate the counter
+
+
+
+  homePageTestimonial = {
+    title: `We <span style="color:#006cb6 ">Excel</span> In`,
+    subTitle: "TESTIMONIALS",
+  };
+
+  safeServiceTitle: SafeHtml;
+  safeTestimonialTitle: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.safeServiceTitle = this.sanitizer.bypassSecurityTrustHtml(this.homePageService.title);
+    this.safeTestimonialTitle = this.sanitizer.bypassSecurityTrustHtml(this.homePageTestimonial.title);
+  }
+  homePageAboutUs = {
+    subTitle: "GET TO KNOW US",
+    title: "Empowering Your Digital Growth with Offshore",
+    subDescription: "IT Solutions",
+    description: "With 26+ years of expertise, Notetech is a leader in offshore software development, delivering high-quality IT solutions to clients in the USA, UK, and Europe. Our dedicated teams help reduce operational costs and enhance efficiency with cutting-edge technology and a commitment to excellence."
+  }
+
+  homePageService = {
+    title: `What We <span style="color:#006cb6 ">Offer</span>`,
+    subTitle: "OUR SERVICES",
+    service: [
+      { icon: "fas fa-pen-fancy", title: "Project Creation", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+      { icon: "fas fa-dharmachakra", title: "Software Development", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+      { icon: "fas fa-tasks", title: "Project Management", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+      { icon: "fas fa-tachometer-alt", title: "Project Implementation", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+      { icon: "fas fa-recycle", title: "Software Update", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+      { icon: "fas fa-headset", title: "24/7 Support", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." }
+    ]
+  }
+
+
+
   @ViewChild('counterElement', { static: false }) counterElement!: ElementRef;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    const maxScroll = 300; 
-    this.glowIntensity = Math.max(0, 1 - scrollTop / maxScroll); 
+    const maxScroll = 300;
+    this.glowIntensity = Math.max(0, 1 - scrollTop / maxScroll);
   }
 
   getGlowStyle(): string {
-    const intensity = this.glowIntensity; 
+    const intensity = this.glowIntensity;
     return `
       0 0 ${7 * intensity}px #05f1f9,
       0 0 ${10 * intensity}px #05f1f9,
